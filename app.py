4b7ed9ae706c9b22c22367ecc8a090891c9d29c5
sarv8b <72740977+sarv8b@users.noreply.github.com>
2023-04-19 23:26:53 +0530

    updated script

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-import streamlit as st 
+import streamlit as st
 
-A=st.number_input(int(input("Enter first number: ")))
-B=st.number_input(int(input("Enter second number: ")))
-C=st.number_input(int(input("Enter third number: ")))
-if (A >= B) and (A >= C):
- largest = A
-elif (B >= A) and (B >= C):
- largest = B
-else:
- largest = C
-st.write("The largest number between",A,",",B,"and",C,"is",largest)
+def largest_of_three(a, b, c):
+    largest = a
+    if b > largest:
+        largest = b
+    if c > largest:
+        largest = c
+    return largest
+
+st.title("Largest of Three Numbers")
+st.write("Enter three numbers below:")
+
+a = st.number_input("First number:", value=0)
+b = st.number_input("Second number:", value=0)
+c = st.number_input("Third number:", value=0)
+
+if st.button("Calculate"):
+    result = largest_of_three(a, b, c)
+    st.write("The largest number is:", result)
